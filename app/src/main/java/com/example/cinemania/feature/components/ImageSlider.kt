7ad4112f34eb2a6a9f7fee0d2a71c75c0e4b3be6
alzh-azin <alzh.azin@gmail.com>
2024-05-22 22:03:13 +0330

    Implement trend movies slider

---
+++ app/src/main/java/com/example/cinemania/feature/components/ImageSlider.kt
@@ -0,0 +1,141 @@
+package com.example.cinemania.feature.components
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.PagerSnapDistance
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
+import coil.compose.rememberAsyncImagePainter
+import kotlinx.coroutines.launch
+import kotlin.math.absoluteValue
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ImageSlider(
+    images: List<String?>,
+    modifier: Modifier = Modifier
+) {
+
+    val scope = rememberCoroutineScope()
+
+    val interactionSource = remember { MutableInteractionSource() }
+
+    val itemSpacing = 16.dp
+    val pagerState = rememberPagerState {
+        images.size
+    }
+
+    Box(modifier = modifier.fillMaxSize()) {
+
+
+        HorizontalPager(
+            state = pagerState, modifier = modifier,
+            flingBehavior = PagerDefaults.flingBehavior(
+                state = pagerState,
+                pagerSnapDistance = PagerSnapDistance.atMost(0)
+            ),
+            contentPadding = PaddingValues(horizontal = 48.dp),
+            pageSpacing = itemSpacing
+        ) { currentPage ->
+            val painter = rememberAsyncImagePainter(model = images[currentPage % images.size])
+
+            Card(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .aspectRatio(9 / 16f)
+                    .padding(vertical = 64.dp)
+                    .graphicsLayer {
+                        val pageOffSet = (
+                                (pagerState.currentPage - currentPage) + pagerState
+                                    .currentPageOffsetFraction
+                                ).absoluteValue
+                        alpha = lerp(
+                            start = 0.5f,
+                            stop = 1f,
+                            fraction = 1f - pageOffSet.coerceIn(0f, 1f)
+                        )
+                        scaleY = lerp(
+                            start = 0.75f,
+                            stop = 1f,
+                            fraction = 1f - pageOffSet.coerceIn(0f, 1f)
+                        )
+                    },
+                elevation = CardDefaults.cardElevation(8.dp)
+            ) {
+                Image(
+                    painter = painter,
+                    contentDescription = null,
+                    modifier = modifier
+                        .fillMaxSize()
+                        .clickable(
+                            interactionSource = interactionSource,
+                            indication = null,
+                            enabled = true,
+                        ) {
+                            scope.launch {
+                                pagerState.animateScrollToPage(currentPage)
+                            }
+                        },
+                    contentScale = ContentScale.FillBounds
+
+                )
+            }
+
+
+        }
+
+
+    }
+
+
+//    val pagerState = rememberPagerState {
+//        images.size
+//    }
+//
+//    HorizontalPager(
+//        state = pagerState,
+//        modifier = modifier
+//            .fillMaxSize(),
+//
+//        ) { currentPage ->
+//
+//        Card(
+//            modifier = modifier
+//                .fillMaxSize()
+//                .padding(26.dp),
+//            elevation = CardDefaults.cardElevation(8.dp)
+//        ) {
+//
+//            val painter = rememberAsyncImagePainter(model = images[currentPage % images.size])
+//
+//            Image(
+//                painter = painter,
+//                contentDescription = null,
+//                modifier = modifier
+//                    .fillMaxSize(),
+//                contentScale = ContentScale.FillBounds
+//
+//
+//            )
+//        }
+//    }
+}
--- app/src/main/java/com/example/cinemania/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/cinemania/feature/home/HomeScreen.kt
@@ -1,21 +1,11 @@
 package com.example.cinemania.feature.home
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import coil.compose.rememberAsyncImagePainter
 import com.example.cinemania.core.domain.model.Media
+import com.example.cinemania.feature.components.ImageSlider
 
 @Composable
 fun HomeRoute(homeViewModel: HomeViewModel = hiltViewModel()) {
@@ -28,41 +18,8 @@ fun HomeRoute(homeViewModel: HomeViewModel = hiltViewModel()) {
 @Composable
 fun HomeScreen(trendMedia: List<Media>) {
 
-    TrendMediaList(trendMedia)
-}
-
-@Composable
-fun TrendMediaList(
-    trendMedia: List<Media>,
-    modifier: Modifier = Modifier,
-) {
-
-    LazyColumn(modifier) {
-        items(items = trendMedia, key = { media -> media.id }) {
-            MediaItem(name = it.title, image = it.posterPath)
-        }
-    }
-}
-
-@Composable
-fun MediaItem(
-    name: String?,
-    image: String?,
-    modifier: Modifier = Modifier
-) {
-
-    val painter = rememberAsyncImagePainter(model = image)
-
-    Row(modifier.padding(top = 16.dp)) {
-
-        Image(
-            painter = painter,
-            contentDescription = null,
-            modifier = Modifier
-                .height(100.dp)
-                .width(70.dp)
-        )
-
-        Text(text = name.orEmpty())
-    }
+    if (trendMedia.isNotEmpty())
+        ImageSlider(trendMedia.map {
+            it.posterPath
+        })
 }
